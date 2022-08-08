a613b86812a9f4509ecda860ee002a56d350543d
peterson oaikhenah <oaikhenahpeterson@gmail.com>
2022-08-08 04:02:13 +0100

    update upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="nextwebb/mldocker"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username=nextwebb --password=$DOCKER_PASSWORD
+docker tag mldocker $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
