b3a7eec6432e257398dd98eae088b0308916b382
peterson oaikhenah <oaikhenahpeterson@gmail.com>
2022-08-08 06:01:04 +0100

    deploy to kubernetes and portforwarding to 8000

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,15 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
-
+dockerpath="nextwebb/mldocker:latest"
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deploy mldocker --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward pod/$(echo $(kubectl get pods) | awk -F ' ' '{print $6}') --address 0.0.0.0 8000:80
